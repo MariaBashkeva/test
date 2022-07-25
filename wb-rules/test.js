cbb87dc2e0e8d463425aacce8a32d18e1f6bede5
MariaBashkeva <bashkevam@yandex.ru>
2022-07-25 11:37:18 +0300

    define rule following the change A1_IN

--- wb-rules/test.js
+++ wb-rules/test.js
@@ -1,10 +1,17 @@
 defineVirtualDevice("HAVAC_test",{
     title: "EXT1_R3A1",
     cells: {
-        enabled: {
+        EXT1_R3A1: {
             type: "switch",
             value: dev["wb-gpio/A1_IN"]
         }
     }
 
 });
+defineRule({
+    whenChanged: "HAVAC_test/EXT1_R3A1",
+    then: function(){
+        log('ALARM A1_IN')
+    }
+});
+
