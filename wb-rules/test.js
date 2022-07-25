8cd3abf6e3329e32ff55aa5186c353ef16c69a37
MariaBashkeva <bashkevam@yandex.ru>
2022-07-25 12:04:34 +0300

    define a condition for Channel1

--- wb-rules/test.js
+++ wb-rules/test.js
@@ -8,10 +8,26 @@ defineVirtualDevice("HAVAC_test",{
     }
 
 });
+
 defineRule({
     whenChanged: "HAVAC_test/EXT1_R3A1",
     then: function(){
         log('ALARM A1_IN')
+        setTimeout(function() {
+            if (!dev["wb-gpio/A2_IN"]){
+                dev["HAVAC_test/EXT1_R3A1"]=false
+                log('ALARM A2_IN')
+            }
+        }, 20000)
     }
 });
 
+if (dev["HAVAC_test/EXT1_R3A1"]){
+    if (dev["wb-w1/28-00000d6b460c"]>25){
+        dev["wb-mao4_209/Channel1"]=8000
+    }else {
+        dev["wb-mao4_209/Channel1"]=2000
+    }
+}else {
+    dev["wb-mao4_209/Channel1"]=0
+}
